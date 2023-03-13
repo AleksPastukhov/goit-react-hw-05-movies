4cd3134322810796a144a318482bcb932a8ec667
AleksPastukhov <passandro1990@gmail.com>
2023-03-13 14:36:24 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  background-color: ${p => p.theme.colors.backgroundColor}; ;
+  background-color: ${p => p.theme.colors.backgroundColor};
 `;
